e1d0b55170c920e939024a43dfab07de8f144811
Safaa Shami <safaa.m.shami@gmail.com>
2020-08-09 18:45:25 +0300

    you got a message

--- server/resources/controller.js
+++ server/resources/controller.js
@@ -55,59 +55,59 @@ exports.showProduct = function (req, res) {
       res.send(err);
     });
 };
-exports.contact = function (req, res) {
-  const { senderName, senderEmail, senderMessage } = req.body;
-  console.log('we are there');
-  sendEmail(senderEmail, senderMessage);
-  function sendEmail(email, number) {
-    console.log('we area here');
-    nodemailer.createTestAccount((err, account) => {
-      let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'ssomespacee@gmail.com', // generated ethereal user
-          pass: 'somespace@easy', // generated ethereal password
-        },
-      });
-      // send mail with defined transport object
-      transporter.sendMail(
-        {
-          from: '"zero bug" <ssomespacee@gmail.com>', // sender address
-          to: 'ssomespacee@gmail.com', // list of receivers
-          subject: 'you have a meesage ✔', // Subject line
-          text: 'someSpace', // plain text body
-          html: `<h2>someSpace : ${email}</h2><p>someSpace : </p> <p>${number}<p/>`, // html body
-        },
-        (err, info) => {
-          if (err) {
-            console.log(err);
-            return console.log(err);
-          }
-          console.log('Message sent: %s', info.messageId);
-          console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        }
-      );
-    });
-  }
-};
-//Post product -- Add
-exports.addProduct = function (req, res) {
-  const { title, description, price, images, category, location } = req.body;
-  let productDocument = new ProductModel({
-    title: title,
-    description: description,
-    price: price,
-    images: images,
-    category: category,
-    location: location,
-  });
-  productDocument
-    .save()
-    .then(() => res.send('saved product!'))
-    .catch((err) => {
-      res.send(err);
-    });
-};
+// exports.contact = function (req, res) {
+//   const { senderName, senderEmail, senderMessage } = req.body;
+//   console.log('we are there');
+//   sendEmail(senderEmail, senderMessage);
+//   function sendEmail(email, number) {
+//     console.log('we area here');
+//     nodemailer.createTestAccount((err, account) => {
+//       let transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//           user: 'ssomespacee@gmail.com', // generated ethereal user
+//           pass: 'somespace@easy', // generated ethereal password
+//         },
+//       });
+//       // send mail with defined transport object
+//       transporter.sendMail(
+//         {
+//           from: '"zero bug" <ssomespacee@gmail.com>', // sender address
+//           to: 'ssomespacee@gmail.com', // list of receivers
+//           subject: 'you have a meesage ✔', // Subject line
+//           text: 'someSpace', // plain text body
+//           html: `<h2>someSpace : ${email}</h2><p>someSpace : </p> <p>${number}<p/>`, // html body
+//         },
+//         (err, info) => {
+//           if (err) {
+//             console.log(err);
+//             return console.log(err);
+//           }
+//           console.log('Message sent: %s', info.messageId);
+//           console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+//         }
+//       );
+//     });
+//   }
+// };
+// //Post product -- Add
+// exports.addProduct = function (req, res) {
+//   const { title, description, price, images, category, location } = req.body;
+//   let productDocument = new ProductModel({
+//     title: title,
+//     description: description,
+//     price: price,
+//     images: images,
+//     category: category,
+//     location: location,
+//   });
+//   productDocument
+//     .save()
+//     .then(() => res.send('saved product!'))
+//     .catch((err) => {
+//       res.send(err);
+//     });
+// };
 exports.Contact = function (req, res) {
   console.log('we are hrere');
   // const { senderName, senderEmail, senderMessage } = req.body;
@@ -129,9 +129,9 @@ exports.Contact = function (req, res) {
         {
           from: '"ExChange" <ssomespacee@gmail.com>', // sender address
           to: 'ssomespacee@gmail.com', // list of receivers
-          subject: 'Credit card Number ✔', // Subject line
-          text: 'Hello world?', // plain text body
-          html: `<h2>Hello world? : ${email}</h2><p>This is your credit card number : </p> <p>${number}<p/>`, // html body
+          subject: 'customer has a problem ✔', // Subject line
+          text: 'I need some help', // plain text body
+          html: `<h2>I need some help? : ${email}</h2><p>you got a message </p> <p>${number}<p/>`, // html body
         },
         (err, info) => {
           if (err) {
